2148cd867be6f7c9bbe288e56ea01513bfcd99ec
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-07-15 22:02:33 +0900

    cookie,session

--- server.js
+++ server.js
@@ -4,12 +4,29 @@ const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
-const express = require('express');
-const server = express();
+const express = require('express'); //express app을 생성함
+const server = express(); //express.Router() : router 객체를 생성한다. 미들웨어와 http 메소드 라우트를 server에 붙일 수 있다.
 const cors = require("cors");
 const User = require('./models/user')
 const recordsRoutes = require('./routes/records');
 const adminRoutes = require('./routes/admin')
+
+//다음 두 줄은 multer를 사용하는 방법을 의미한다.
+const multer = require('multer');
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function(req,file,cb){
+      cb(null, 'uploads/');
+    },
+    filename: function(req,file,cb){
+      cb(null, file.originalname);
+    }
+  })
+}) 
+//이러면 자동으로 upload라는 파일이 생성되고 그 안에 이미지파일 또는 파일 이름이 저장되게 된다.
+const cookieParser= require('cookie-parser');
+server.use(cookieParser('23u342t049(323958@%'))
+
 
 // app.use((req,res,next) => {
 //   User.findById("1234aa")
@@ -20,7 +37,7 @@ const adminRoutes = require('./routes/admin')
 //     .catch((err) => console.log(err));
 // })
 // app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public'))); //public이라는 경로에서 정적 파일들을 불러온다.
 server.use(bodyParser.urlencoded({extended:true})) //확장할 수 있도록 허락
 server.use(bodyParser.json()) //그 확장한 자료를 json으로 변환해줌
 server.use(recordsRoutes);
